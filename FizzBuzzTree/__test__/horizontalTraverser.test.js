5cf38340b256d1b27650f543291ca259d5702969
Panos Akio Kumasaka <panosrockbear@gmail.com>
2018-05-08 19:14:01 -0700

    done; git commit -m done

---
+++ FizzBuzzTree/__test__/horizontalTraverser.test.js
@@ -0,0 +1,13 @@
+const Tree=require('../horizontalTraverser')
+
+let Maple=new Tree();
+
+Maple.head=new Maple.Node(1);
+Maple.head.left=new Maple.Node(2);
+Maple.head.left.left=new Maple.Node(3);
+Maple.head.left.right=new Maple.Node(5)
+Maple.head.right=new Maple.Node(15);
+test('testing horizontal traversal',()=>{
+    let result=Maple.traverser();
+    expect(result).toEqual(` 1 2 3 5 15`);
+});
