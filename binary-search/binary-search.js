9f3c85110b0bc0b73e86e201154334b178439988
Panos <panosrockbear@gmail.com>
2018-05-12 03:20:29 -0700

    finished second time

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,5 +1,28 @@
-function binary-search(arr,var){
-    for(let i=0;i<arr[arr.length-1];i++)
-    {if(arr[i]===var){return(i)}}
-    return -1;
+function array_adjacent_product(inputArray)
+{
+  let largest=0;
+    for(let i=0;i<inputArray.length;i++){
+      if(inputArray[i])
+        {for(let j = 0;j<inputArray[i].length;j++){
+          if(inputArray[i][j])
+            {
+              let currentValue=inputArray[i][j];
+            if(inputArray[i][j+1])
+              {if(inputArray[i][j]*inputArray[i][j+1]>largest)
+                {largest=inputArray[i][j]*inputArray[i][j+1]}
+              }
+              if(inputArray[i+1]){
+                if(inputArray[i+1][j])
+                  {if(inputArray[i+1][j]*inputArray[i][j]>largest)
+                    {largest=inputArray[i+1][j]*inputArray[i][j]
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return largest;
 }
+
+module.exports=array_adjacent_product;
