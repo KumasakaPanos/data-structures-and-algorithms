f678ffc4110ef6021cd7d2f7e4fa7c85b34afd91
Panos <panosrockbear@gmail.com>
2018-05-14 00:10:46 -0700

    done

--- __test__/linked-list.test.js
+++ __test__/linked-list.test.js
@@ -7,5 +7,11 @@ console.log(linkedList);
 test('tree testing',()=>{
     let testTree=new linkedList(1);
     testTree.append(2);
-    expect(testTree.kthFromEnd(2)).toEqual(2);
+    let testTree2=new linkedList(3);
+    let testTree3=testTree.mergeList(testTree2);
+    console.log(testTree3.head.data)
+    console.log(testTree3.head);
+    expect(testTree3.head.data).toEqual(1);
+    expect(testTree3.head.next.data).toEqual(3);
+    expect(testTree3.head.next.next.data).toEqual(2);
 })
