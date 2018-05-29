cad11d9cadcf453c37ae6415ca4961c8d00533dc
Panos Akio Kumasaka <panosrockbear@gmail.com>
2018-05-29 15:35:35 -0700

    done

--- solution.js
+++ solution.js
@@ -7,7 +7,7 @@ module.exports=function findCommon(input)
     }
     let storer=new Array;
     input=input.toLowerCase();
-    input=input.replace(/[^0-9a-z\s]/gi, ' ');
+    input=input.replace(/[^0-9a-z\s]/gi, '');
     input=input.split(' ');
     for(let i=0;i<input.length;i++)
     {
