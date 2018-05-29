f093d36dfb15a0979eda3de110e4ca8c5509040a
Panos Akio Kumasaka <panosrockbear@gmail.com>
2018-05-29 15:31:00 -0700

    done

---
+++ solution.js
@@ -0,0 +1,21 @@
+module.exports=function findCommon(input)
+{
+    console.log(input);
+    if(input == null||input == '')
+    {
+        return('Invalid input');
+    }
+    let storer=new Array;
+    input=input.toLowerCase();
+    input=input.replace(/[^0-9a-z\s]/gi, ' ');
+    input=input.split(' ');
+    for(let i=0;i<input.length;i++)
+    {
+        if(storer[input[i]])
+        {
+            return input[i];
+        }
+        storer[input[i]]=input[i];
+    }
+    return('No matches found');
+}
---
+++ solution.test.js
@@ -0,0 +1,21 @@
+const testFunction = require('./solution');
+
+const testString='The quick brown dog jumped on a lazy fox.'
+const testString1='The peckish pidgeons presumptously perused the packed park.'
+const testString2='Grork brought his club to the club so he could club his dates.'
+
+test('testing no match', ()=>{
+    expect(testFunction(testString)).toEqual('No matches found');
+})
+
+test('testing no input', ()=>{
+    expect(testFunction('')).toEqual('Invalid input')
+})
+
+test('Checking match with different case', ()=>{
+    expect(testFunction(testString1)).toEqual('the')
+})
+
+test('Checking match with same case', () => {
+    expect(testFunction(testString2)).toEqual('club')
+})
